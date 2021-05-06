61fe7090f5b1c8d47dbb52f4f70352a9f7e98177
rivaldofez <rivaldofez@gmail.com>
2021-05-06 09:50:20 +0700

    preparation project

---
+++ Academy/app/src/main/java/com/rivaldofez/academy/data/ContentEntity.kt
@@ -0,0 +1,5 @@
+package com.rivaldofez.academy.data
+
+data class ContentEntity(
+    var content: String?
+)
--- Academy/app/src/main/java/com/rivaldofez/academy/ui/home/HomeActivity.kt
+++ Academy/app/src/main/java/com/rivaldofez/academy/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.rivaldofez.academy
+package com.rivaldofez.academy.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rivaldofez.academy.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
