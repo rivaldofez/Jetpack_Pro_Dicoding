d5539f8bde4c8a1988bb77700198bada8ed2c5ef
rivaldofez <rivaldofez@gmail.com>
2021-05-19 06:14:03 +0700

    create splash screen

--- Moviers/app/src/main/java/com/rivaldofez/moviers/ui/home/HomeActivity.kt
+++ Moviers/app/src/main/java/com/rivaldofez/moviers/ui/home/HomeActivity.kt
@@ -1,11 +1,12 @@
-package com.rivaldofez.moviers
+package com.rivaldofez.moviers.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rivaldofez.moviers.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 }
---
+++ Moviers/app/src/main/java/com/rivaldofez/moviers/ui/splash/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.rivaldofez.moviers.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import com.bumptech.glide.Glide
+import com.rivaldofez.moviers.R
+import com.rivaldofez.moviers.databinding.ActivitySplashBinding
+import com.rivaldofez.moviers.ui.home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+    companion object {
+        private val SPLASH_TIME : Long = 3000
+    }
+
+    private lateinit var splashBinding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        Glide.with(this).load(R.drawable.logo_moviers).into(splashBinding.imgLogoSplash)
+        splashBinding.progressSplash.visibility = View.VISIBLE
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            splashBinding.progressSplash.visibility = View.GONE
+            finish()
+        }, SPLASH_TIME)
+    }
+}
